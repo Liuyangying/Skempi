ab9b89b7d11996e1a5c545b33ee6b6725f19bc5a
jtamwaffle <jzt320@lehigh.edu>
2022-02-22 11:36:39 -0500

    filter by charge: done

--- filter_skempi.py
+++ filter_skempi.py
@@ -5,8 +5,10 @@ import sys
 import argparse
 
 
+# parser function using argparse
+# TODO: allow many different inputs for each type of filter to be done in order of inputs
 def parseArg():
-    # parse output to take two inputs -i
+    # parse output to take inputs
     parser = argparse.ArgumentParser(
         description='Identify all points between two proteins that are within a certain distance of each other.')
     parser.add_argument('-i', nargs='?', metavar='Input_Skempi',
@@ -16,7 +18,7 @@ def parseArg():
     # parser.add_argument('-o', nargs='?', metavar='OutputPDB',
     #                     help='Output PDB file name')
 
-    # parse list of points from inputs
+    # parse input file - if not given, set as default file i
     args = parser.parse_args()
     args = vars(args)
     i = args['i']
@@ -25,19 +27,24 @@ def parseArg():
     return i
 
 
+# function to filter charges. Takes a dataframe input, and the desired start and end charges.
 def charge_filter(combined, start_charge, end_charge):
     combined_filtered = pd.DataFrame().reindex_like(combined)
     mutation = combined['MutantList']
 
+    # loop through mutant list column which contains all mutations
     for i in range(len(mutation)):
-        c1 = 0
-        c2 = 0
-
+        # if multiple mutations exist, split by comma-separator and loop through each to check
         multi_mutations = mutation.iloc[i].split(",")
         # print(multi_mutations)
         for m in multi_mutations:
+            c1 = 0
+            c2 = 0
+            # index [0] is first char in mutation list indicating 1st residue
+            # index [-1] is last char in mutation list indicating 2nd residue
             first = m[0]
             last = m[-1]
+            # check if residues are charged or not
             if first == 'R' or first == 'H' or first == 'K':
                 c1 = 1
             if first == 'D' or first == 'E':
@@ -46,8 +53,11 @@ def charge_filter(combined, start_charge, end_charge):
                 c2 = 1
             if last == 'D' or last == 'E':
                 c2 = -1
+            # if charge of residues are equal to desired charge, write to new dataframe
             if c1 == start_charge and c2 == end_charge:
                 combined_filtered.iloc[i] = combined.iloc[i]
+
+    combined_filtered.dropna(subset=["PDBCode"], inplace=True)
 
     print(combined_filtered)
     return combined_filtered
